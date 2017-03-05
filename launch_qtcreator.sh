194b7914b93d434d22b7c7f7ec486e3b9bee372d
yoyonel <yoyonel@hotmail.com>
2017-03-06 00:00:38 +0100

    [pipeline] Travail sur l'utilisation/l'intégration d'outils d'édition comme Vim, SublimeText et QtCreator.

--- launch_qtcreator.sh
+++ launch_qtcreator.sh
@@ -7,12 +7,12 @@ if [ ! -d li3ds/.volumes/dev/qtcreator/.config ]; then
 fi
 
 # -e NEWUSER=$USER \
-docker run 	-it --rm \
-			--name qtcreator \
-			-h qtcreator \
-            -e DISPLAY=$DISPLAY \
-            -v /tmp/.X11-unix:/tmp/.X11-unix \
-            -v $(realpath li3ds/.volumes/dev/qtcreator/.config):/home/.config \
-            --security-opt seccomp=unconfined \
-            --volumes-from li3ds_ros \
+docker run 	-it --rm 															\
+			--name qtcreator 													\
+			-h qtcreator 														\
+            -e DISPLAY=$DISPLAY 												\
+            -v /tmp/.X11-unix:/tmp/.X11-unix 									\
+            --security-opt seccomp=unconfined 									\
+            -v $(realpath li3ds/.volumes/dev/qtcreator/.config):/home/.config 	\
+            --volumes-from li3ds_ros 											\
             li3ds-dev_qtcreator $@
--- li3ds/pipeline/run_bash_on_image.sh
+++ li3ds/pipeline/run_bash_on_image.sh
@@ -4,13 +4,23 @@ source env_for_project.sh $1
 
 echo_i "Build project from: ${GREEN}$PROJECT_IMAGE_TO"
 
+xhost + 
+OPTIONS_FOR_QTCREATOR="
+			-e DISPLAY=$DISPLAY 												\
+            -v /tmp/.X11-unix:/tmp/.X11-unix 									\
+            -v $(realpath ../dev/qtcreator/.config):/home/.config 				\
+            --security-opt seccomp=unconfined 									\
+            "
+
 # Container sur l'image du projet
-docker	run												\
+	docker	run												\
 		-it --rm										\
 		--name li3ds-prototype_step2					\
 		-v $(realpath scripts):/root/scripts			\
+		-v /var/run/docker.sock:/var/run/docker.sock	\
 		-v $PATH_TO_OVERLAY_WS_VOLUME:/root/overlay_ws 	\
 		-v $PATH_TO_CATKIN_WS_VOLUME:/root/catkin_ws 	\
+		$OPTIONS_FOR_QTCREATOR							\
 		$PROJECT_IMAGE_TO		 						\
 		bash
 
