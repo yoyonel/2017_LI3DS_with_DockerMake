bc11d1ceebcf9d54c6893dab4cea979860a4a20f
ATTY Lionel (IGN) <lionel.atty@ign.fr>
2017-03-06 19:21:08 +0100

    [tools/st3] Travail sur l'intégration de l'éditeur de text Sublime Text 3. Il reste pas mal de travail à faire sur ce package, en particulier la gestion des plugins/settings.

--- li3ds/tools/sublimetext3/run.sh
+++ li3ds/tools/sublimetext3/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
+
+ST3=/usr/src/sublime_text/sublime_text
+
 if [ -z ${NEWUSER+x} ]; then
 	echo 'WARN: No user was defined, defaulting to root.'
 	echo 'WARN: Sublime will save files as root:root.'
 	echo '      To prevent this, start the container with -e NEWUSER=$USER'
-	LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 /usr/src/sublime_text/sublime_text -w
+	LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 $ST3 -w
 else
 	# The root user already exists, so we only need to do something if
 	# a user has been specified.
@@ -13,5 +16,6 @@ else
 	# to the current project (i.e. in the sidebar at start), append
 	# "-a /home/$NEWUSER/Documents" (without quotes) into the su -c command below.
 	# Example: su $NEWUSER -c "/usr/src/sublime_text/sublime_text -w -a /home/$NEWUSER/Documents"
-	su $NEWUSER -c "LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 /usr/src/sublime_text/sublime_text -w"
+	su $NEWUSER -c "LANG=en_US.UTF-8 LC_CTYPE=en_US.UTF-8 \
+					$ST3 -w"
 fi
