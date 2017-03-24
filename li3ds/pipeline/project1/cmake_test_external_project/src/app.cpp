5a777bd1fe09afae94e28807756b780761952f9e
ATTY Lionel <yoyonel@hotmail.com>
2017-03-24 19:21:59 +0100

    [pipeline/project] Mise a jour du projet cmake_test_external.

--- li3ds/pipeline/project1/cmake_test_external_project/src/app.cpp
+++ li3ds/pipeline/project1/cmake_test_external_project/src/app.cpp
@@ -41,24 +41,39 @@ int main(void)
 {
     // Sentence string to be parsed
     // char *sentence = strdup("$GPGLL,4916.45,N,12311.12,W,225444,A\n\n");
-    char *sentence = strdup("$GPRMC,000001,A,4901.00,N,200.00,W,0.1,180,01012016,,,S*6C\n\n");
+    //    char *sentence = strdup("$GPRMC,000001,A,4901.00,N,200.00,W,0.1,180,01012016,,,S*6C\n\n");
+    char *sentence = strdup("$GPRMC,000003,A,4901.00,N,200.00,W,0.1,180,01012016,,,S*6E\n\n");
+
+    const size_t strlen_sentence = strlen(sentence);
 
     printf("Parsing NMEA sentence: %s\n", sentence);
     printf("Type of sentence: %d\n", nmea_get_type(sentence));
-    printf("Has a checksum: %d - %d\n", nmea_get_checksum(sentence)>0, nmea_get_checksum(sentence));
-    printf("Validate sentence: %d\n", nmea_validate(sentence, strlen(sentence), nmea_get_checksum(sentence)>0));
+    printf("Has a checksum: %s - %d\n",
+           nmea_has_checksum(sentence, strlen_sentence) == 0 ? "True" : "False",
+           nmea_get_checksum(sentence));
+    printf("Validate sentence: %s\n",
+           nmea_validate(sentence,
+                         strlen(sentence),
+                         nmea_has_checksum(sentence, strlen_sentence)
+                         ) == 0 ? "True" : "False"
+           );
     // Pointer to struct containing the parsed data. Should be freed manually.
     nmea_s *data;
 
     // Parse...
-    data = nmea_parse(sentence, strlen(sentence), nmea_get_checksum(sentence)>0);
+    data = nmea_parse(sentence,
+                      strlen_sentence,
+                      nmea_has_checksum(sentence, strlen_sentence) == 0);
 
-    if (NMEA_GPGLL == data->type)
-        parse_nmea_gpgll(data);
-    else if (NMEA_GPRMC == data->type)
-        parse_nmea_gprmc(data);
+    if (data) {
+        if (NMEA_GPGLL == data->type)
+            parse_nmea_gpgll(data);
+        else if (NMEA_GPRMC == data->type)
+            parse_nmea_gprmc(data);
 
-    nmea_free(data);
+        nmea_free(data);
+    }
+
     free(sentence);
 
     return 0;
